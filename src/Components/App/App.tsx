83a39f918c1f894f282479d2c75d6eff39b0afb3
Amy S <140124108+amysiu1028@users.noreply.github.com>
2024-02-02 17:08:34 -0500

    remove test file

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //    <h1>Mon-Recette</h1>
-  //   </div>
-  // );
+  return (
+    <div className="App">
+     <h1>Mon-Recette</h1>
+    </div>
+  );
 }
 
 export default App;
