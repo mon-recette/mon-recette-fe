84476e8b922a3605c5f2c1ef7017466eec9d1930
Amy S <140124108+amysiu1028@users.noreply.github.com>
2024-02-07 18:59:35 -0500

    only display unique recipes, no duplicates

--- src/Components/SavedRecipePage/SavedRecipesPage.tsx
+++ src/Components/SavedRecipePage/SavedRecipesPage.tsx
@@ -7,45 +7,52 @@ import ReturnToSearchButton from '../ReturnToSearchButton/ReturnToSearchButton';
 import { Recipe } from '../../types';
 import { getSavedRecipes } from '../apiCalls';
 
-const SavedRecipesPage: React.FC<SavedRecipesPageProps> = ({recipes, savedRecipes, updateSavedRecipes, updateSeletedFilteredRecipe}) => {
-  // const [savedRecipes, setSavedRecipes] = useState<Recipe[]>([]);
-  const id = 1
+// ... (your imports)
+
+const SavedRecipesPage: React.FC<SavedRecipesPageProps> = ({ recipes, savedRecipes, updateSavedRecipes, updateSeletedFilteredRecipe }) => {
+  const id = 1;
+
   useEffect(() => {
     getSavedRecipes(id)
-    .then((savedRecipeData) => {
-      console.log('Received saved recipe data:', savedRecipeData);
-      const allSavedRecipes = savedRecipeData.data.attributes.recipes
-      const userSpecificRecipes = allSavedRecipes.filter(recipe => recipe.user_id === id)
-      console.log('User specific recipes:', userSpecificRecipes);
-      updateSavedRecipes(userSpecificRecipes)
-    });
+      .then((savedRecipeData) => {
+        console.log('Received saved recipe data:', savedRecipeData);
+        const allSavedRecipes = savedRecipeData.data.attributes.recipes;
+        const userSpecificRecipes = allSavedRecipes.filter((recipe) => recipe.user_id === id);
+        console.log('User specific recipes:', userSpecificRecipes);
+        updateSavedRecipes(userSpecificRecipes);
+      });
   }, [id]);
 
   console.log('savedRecipes', savedRecipes);
 
-  const displayUserSpecificSavedRecipes = savedRecipes.flat().map((savedRecipe) => {
+  const uniqueRecipeNamesSet = new Set(savedRecipes.flat().map((recipe) => recipe.name));
+
+  console.log("uniqueRecipeNamesSet", uniqueRecipeNamesSet);
+
+  const uniqueRecipes = Array.from(uniqueRecipeNamesSet).map((recipeName) => {
+    const firstMatchingRecipe = savedRecipes.find((recipe) => recipe.name === recipeName);
+
     return (
       <SavedRecipeCard
-        key={savedRecipe.name}
-        name={savedRecipe.name}
-        instructions={savedRecipe.instructions}
-        image_url={savedRecipe.image_url}
-        ingredients={savedRecipe.ingredients}
+        key={recipeName}
+        name={recipeName}
+        instructions={firstMatchingRecipe?.instructions || ''}
+        image_url={firstMatchingRecipe?.image_url || ''}
+        ingredients={firstMatchingRecipe?.ingredients || []}
         recipes={recipes}
         updateSeletedFilteredRecipe={updateSeletedFilteredRecipe}
-        // isSaved={true}
-        // toggleSavedRecipes={() => toggleSavedRecipes(savedRecipe)}
       />
-    )
-  })
-  
-console.log("displayUserSpecificSavedRecipes ",displayUserSpecificSavedRecipes )
+    );
+  });
+
+  console.log("uniqueRecipes", uniqueRecipes);
+
   return (
     <main className='saved-recipes-page'>
       <Header />
       <ReturnToSearchButton />
-      {displayUserSpecificSavedRecipes.length > 0 ? (
-        displayUserSpecificSavedRecipes 
+      {uniqueRecipes.length > 0 ? (
+        uniqueRecipes
       ) : (
         <p>No saved recipes found! Add some!</p>
       )}
@@ -55,3 +62,57 @@ console.log("displayUserSpecificSavedRecipes ",displayUserSpecificSavedRecipes )
 };
 
 export default SavedRecipesPage;
+
+
+// const SavedRecipesPage: React.FC<SavedRecipesPageProps> = ({recipes, savedRecipes, updateSavedRecipes, updateSeletedFilteredRecipe}) => {
+//   // const [savedRecipes, setSavedRecipes] = useState<Recipe[]>([]);
+//   const id = 1
+//   useEffect(() => {
+//     getSavedRecipes(id)
+//     .then((savedRecipeData) => {
+//       console.log('Received saved recipe data:', savedRecipeData);
+//       const allSavedRecipes = savedRecipeData.data.attributes.recipes
+//       const userSpecificRecipes = allSavedRecipes.filter(recipe => recipe.user_id === id)
+//       console.log('User specific recipes:', userSpecificRecipes);
+//       updateSavedRecipes(userSpecificRecipes)
+//     });
+//   }, [id]);
+
+//   console.log('savedRecipes', savedRecipes);
+
+//   const displayUserSpecificSavedRecipes = savedRecipes.flat().map((savedRecipe) => {
+//     return (
+//       <SavedRecipeCard
+//         key={savedRecipe.name}
+//         name={savedRecipe.name}
+//         instructions={savedRecipe.instructions}
+//         image_url={savedRecipe.image_url}
+//         ingredients={savedRecipe.ingredients}
+//         recipes={recipes}
+//         updateSeletedFilteredRecipe={updateSeletedFilteredRecipe}
+//         // isSaved={true}
+//         // toggleSavedRecipes={() => toggleSavedRecipes(savedRecipe)}
+//       />
+//     )
+//   })
+//   // const uniqueRecipes = [...new Set(displayUserSpecificSavedRecipes)];
+//   const uniqueRecipes = [...new Set(displayUserSpecificSavedRecipes)];
+
+//   console.log("uniqueRecipes",uniqueRecipes)
+
+// console.log("displayUserSpecificSavedRecipes ",displayUserSpecificSavedRecipes )
+//   return (
+//     <main className='saved-recipes-page'>
+//       <Header />
+//       <ReturnToSearchButton />
+//       {uniqueRecipes.length > 0 ? (
+//         uniqueRecipes 
+//       ) : (
+//         <p>No saved recipes found! Add some!</p>
+//       )}
+//       <ScrollToTopButton />
+//     </main>
+//   );
+// };
+
+// export default SavedRecipesPage;
