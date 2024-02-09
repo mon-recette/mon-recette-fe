a76ee5af76212504fa92e1316c8971023bac7304
Amy S <140124108+amysiu1028@users.noreply.github.com>
2024-02-09 00:41:02 -0500

    fix toggle, add image, and edit instructions

--- src/Components/SavedRecipeCard/SavedRecipeCard.tsx
+++ src/Components/SavedRecipeCard/SavedRecipeCard.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import './SavedRecipeCard.css'
-import { RecipesData, SavedRecipeCardProps } from '../../types';
-import CookingPot from '../../Images /Cooking-pot.jpg'
-import { useNavigate } from 'react-router-dom';
+import { SavedRecipeCardProps } from '../../types';
+import CookingPot from '../../Images /Cooking-pot.jpg';
+
 
 const SavedRecipeCard: React.FC<SavedRecipeCardProps> =
-  ({ recipes, name, image_url, updateSeletedFilteredRecipe }) => {
+  ({ recipes, ingredients, instructions, name, image_url, updateSeletedFilteredRecipe }) => {
     const [showDropdown, setShowDropdown] = useState(false);
     const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
@@ -23,6 +23,7 @@ const SavedRecipeCard: React.FC<SavedRecipeCardProps> =
 //     updateSeletedFilteredRecipe(singleFilteredRecipe)
 //     navigate(`/filteredRecipes/${name}`); // Update path here
 //   }
+
   return (
     <div className='small-recipe-card'>
       <h3>{name}</h3>
@@ -37,7 +38,19 @@ const SavedRecipeCard: React.FC<SavedRecipeCardProps> =
             ))}
           </ul>
           <h4>Instructions</h4>
-          <p>{instructions}</p>
+          {Array.isArray(instructions) ? (
+            instructions.map((instruction, index) => (
+              <div key={index}>{index + 1}. {instruction}</div>
+            ))
+          ) : (
+            <div>{instructions}</div>
+          )}
+          
+          {image_url ? (
+            <img src={image_url} alt={`Image of ${name}`} />
+          ) : (
+            <img src={CookingPot} alt="Cooking Pot" />
+          )}
         </div>
       )}
     </div>
@@ -46,36 +59,3 @@ const SavedRecipeCard: React.FC<SavedRecipeCardProps> =
 
 export default SavedRecipeCard;
 
-
-// import './SavedRecipeCard.css'
-// import { RecipesData, smallRecipePageProps } from '../../types';
-// import CookingPot from '../../Images /Cooking-pot.jpg'
-// import { useNavigate } from 'react-router-dom';
-
-// const SavedRecipeCard = ({ recipes, name, instructions, image_url, ingredients, updateSeletedFilteredRecipe }) => {
-//   console.log("recipes outside function",recipes)
-
-// //for now, we aren't going to navigate from the savedRecipe card to the more details page (I'd have to refactor the routing for this... and becuase we have one for webscrape and one from filteredpage to singleFilteredPage, I can't figure it out now. But maybe the routes will have to match somehow with all of them which I think is possible in Part 2)
-// //   const navigate = useNavigate()
-
-// //   const moreDetailClickHandler = (event, name: string, recipes: RecipesData) => {
-// //     event.preventDefault()
-// //     // console.log
-// //     console.log("recipes",recipes)
-// //     const singleFilteredRecipe = recipes.data.attributes.recipes.find((recipe) => recipe.name.includes(name))
-// //     updateSeletedFilteredRecipe(singleFilteredRecipe)
-// //     navigate(`/filteredRecipes/${name}`); // Update path here
-// //   }
-
-//   return (
-//     //
-//     <div className='small-recipe-card'>
-//       <h3>{name}</h3>
-
-//       {/* <button type="submit" onClick={(event) => moreDetailClickHandler(event, name, recipes)}>More Details</button>
-//       {/* <button>Save Button for a delete and post?</button> */}  
-//     </div>
-//   );
-// };
-
-// export default SavedRecipeCard;
