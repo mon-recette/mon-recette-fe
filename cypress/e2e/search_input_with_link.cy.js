7402fbd4c3eb662af2de1a4bd996a9a8e59d9b70
Sooyung Kim <kimx1279@gmail.com>
2024-02-26 18:49:08 -0700

    Add search input with link test

--- cypress/e2e/search_input_with_link.cy.js
+++ cypress/e2e/search_input_with_link.cy.js
@@ -1,36 +1,48 @@
 describe('Should display ingredients and instructions', () => {
   beforeEach(() => { 
-    cy.intercept('GET', 'http://localhost:3000/home/https%3A%2F%2Fwww.awickedwhisk.com%2Fhomemade-chicken-ravioli-recipe-3%2F', {
+    cy.intercept('GET', 'https://mon-recette-be-8176efe67145.herokuapp.com/api/v1/searches?term=https://www.awickedwhisk.com/homemade-chicken-ravioli-recipe-3/', {
     statusCode: 200,
     fixture:'awickedwhisk_chicken.json',
   })
   
   })
 
-  it('should return ingredients and instructions when link is provided', () => {
+  it('should return ingredients and instructions when link is provided, and saves the recipe', () => {
     cy.visit('http://localhost:3000/home')
       .get('.search-input')
       .type('https://www.awickedwhisk.com/homemade-chicken-ravioli-recipe-3/')
       .get('.search-button')
       .click()
-    cy.intercept('GET', 'http://localhost:3000/home/https%3A%2F%2Fwww.awickedwhisk.com%2Fhomemade-chicken-ravioli-recipe-3%2F', {
-        statusCode: 200,
-        fixture:'awickedwhisk_chicken.json',
-      })
-      
-      .url()
+
+    cy.url()
       .should('contain', 'www.awickedwhisk.com');
 
-    cy.wait(1000);
-    
     cy.get('h2')
       .should('contain', 'Saved Homemade Chicken Ravioli Recipe')
     
     cy.get('.scroll')
-      .children().should('have.length', 2)
+      .children()
       .first()
-      .within(() => {
-      });
-    cy.children().should('have.length', 44)
+      .should('contain', 'Ingredients')
+      .should('contain', 'Ravioli Dough')
+      .should('contain', '300 large eggs')
+
+    cy.get('.scroll')
+      .children()
+      .next()
+      .should('contain', 'Instruction')
+      .should('contain', 'Make dough')
+      .should('contain', 'Make chicken')
+      .should('contain', 'Mix them together')
+      .should('contain', 'Remove and serve with your favorite sauce.')
+
+    cy.get('.scroll')
+      .children()
+      .next()
+      .get('.saved_btn')
+      .click()
+    
+    cy.get('h3')
+      .should('contain', 'Recipe has been saved')
   })
 });
